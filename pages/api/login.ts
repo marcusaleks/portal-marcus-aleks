96cf25eba24d7544b2b245db83c8bb9dbb9f5024
Marcus S. Aleksandravicius <github@marcus.aleks.nom.br>
2026-03-09 21:44:38 -0300

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -3,6 +3,8 @@ export default async function handler(req: any, res: any) {
   const { key } = req.body;
   const allowed = process.env.ALLOWED_KEYS || ""; 
   const keysArray = allowed.split(',').map(k => k.trim());
-  if (keysArray.includes(key)) { return res.status(200).json({ authorized: true }); }
+  if (keysArray.includes(key)) {
+    return res.status(200).json({ authorized: true });
+  }
   return res.status(401).json({ authorized: false });
 }
