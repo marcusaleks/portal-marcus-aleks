faedf0c1e4d5ea6349d149dc745b6232c5c2c191
Marcus S. Aleksandravicius <github@marcus.aleks.nom.br>
2026-03-09 21:18:14 -0300

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,11 @@
 export default async function handler(req: any, res: any) {
   if (req.method !== 'POST') return res.status(405).end();
-  
   const { key } = req.body;
-  const allowed = process.env.ALLOWED_KEYS || ""; // Lê a variável protegida da Vercel
+  const allowed = process.env.ALLOWED_KEYS || ""; 
   const keysArray = allowed.split(',').map(k => k.trim());
 
   if (keysArray.includes(key)) {
     return res.status(200).json({ authorized: true });
   }
-  
   return res.status(401).json({ authorized: false });
 }
